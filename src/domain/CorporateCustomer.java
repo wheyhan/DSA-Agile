6c015f387c59d4e6d35f45e72f0dd1fc6476fcf5
chbee <chbee@DESKTOP-CQJU6OR>
2018-11-14 11:17:24 +0800

    As a staff, i want to create a  credit limit for corporate customer, so their overdue and bad dept can be avoided.

---
+++ src/domain/CorporateCustomer.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author chbee
+ */
+public class CorporateCustomer extends Buyer{
+    
+    private String CompanyName;
+    private String CompanyAddress;
+   
+    public CorporateCustomer() {
+       
+    }
+    public CorporateCustomer(String id, String name, String ic) {
+        super(id, name, ic);
+    }
+
+    public String getCompanyName() {
+        return CompanyName;
+    }
+
+    public void setCompanyName(String CompanyName) {
+        this.CompanyName = CompanyName;
+    }
+
+    public String getCompanyAddress() {
+        return CompanyAddress;
+    }
+
+    public void setCompanyAddress(String CompanyAddress) {
+        this.CompanyAddress = CompanyAddress;
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/domain/Customer.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author chbee
+ */
+public class Customer extends Buyer{
+
+    private String Address;
+    public Customer() {
+        
+    }
+    public Customer(String id, String name, String ic) {
+        super(id, name, ic);
+        
+    }
+    
+   
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+   
+    /*public static void main(String args[]){
+        Customer cust = new Customer("aaa","bbb","ccc");
+        System.out.println(cust.getName());
+    }*/
+
+    
+    
+
+    
+    
+    
+    
+}
